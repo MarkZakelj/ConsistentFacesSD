a99cf947496a96d3a2912b5da8948ea2772a541a
MarkZakelj <markzakelj@gmail.com>
2024-06-29 20:03:43 +0200

    comfy send request

---
+++ comfy_requests.py
@@ -0,0 +1,83 @@
+import asyncio
+import json
+import os
+from io import BytesIO
+
+import requests
+import websockets
+from PIL import Image
+from dotenv import load_dotenv
+from loguru import logger
+
+from utils import SequentialTimer
+from utils import get_exception_traceback_str
+
+load_dotenv()
+
+JSONHEADERS = {"Content-Type": "application/json"}
+
+ws_uri_template = os.environ["WS_URI_TEMPLATE"]
+comfy_url_template = os.environ["COMFY_URL_TEMPLATE"]
+
+
+async def websocket_get_image(prompt_id: str, hostaddr: str, client_id: str, verbose=False):
+    imgs = []
+    uri = ws_uri_template.format(hostaddr, client_id)
+    async with websockets.connect(uri, max_size=2 ** 25, close_timeout=100) as ws:
+        st = SequentialTimer()
+        while True:
+            # error is thrown if timeout - happens when comfy receives identical payload
+            # and caches even the websocket send image node-nothing is sent
+            out = await asyncio.wait_for(ws.recv(), timeout=90)
+
+            if isinstance(out, str):
+                message = json.loads(out)
+                if message["type"] == "executing":
+                    data = message["data"]
+                    if data["node"] is None and data["prompt_id"] == prompt_id:
+                        st.time("END")
+                        break  # Execution is done
+                if "node" in message["data"]:
+                    st.time(message["data"]["node"])
+                    if verbose:
+                        print(message["data"]["node"])
+                else:
+                    st.time(str(message))
+            elif isinstance(out, bytes):
+                bts = BytesIO(out[8:])  # remove header from payload
+                img = Image.open(bts)
+                imgs.append(img)
+                continue
+    if verbose:
+        print("FINISHED")
+    return imgs, st.get_json_merged()
+
+
+async def comfy_send_request(payload, req_id):
+    attempts = 0
+    imgs = []
+    target_addr = ""
+    timings = {}
+    try:
+        response = requests.post(comfy_url_template.format(target_addr), json=payload, headers=JSONHEADERS)
+        if response.status_code != 200:
+            logger.error(
+                "RequestFailed with non 200 status code, get new address\nSTATUS CODE: {status_code}\nERROR: {error}",
+                status_code=response.status_code,
+                error=response.text,
+            )
+        else:
+            res = response.json()
+            prompt_id = res["prompt_id"]
+            imgs, timings = await websocket_get_image(prompt_id, target_addr, req_id)
+            if len(imgs) == 0:
+                logger.error(
+                    "No images received from comfy, TIMINGS:\n{timings}",
+                    timings=timings
+                )
+    except Exception as e:
+        logger.error(
+            "RequestFailed, ERROR:\n{error}",
+            error=str(get_exception_traceback_str(e)),
+        )
+    return imgs, timings
