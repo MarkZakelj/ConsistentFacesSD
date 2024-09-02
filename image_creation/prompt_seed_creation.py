d6657de272f82c0038bdc106b2d4aa82fcbc9776
MarkZakelj <markzakelj@gmail.com>
2024-09-02 18:30:34 +0200

    add img codes to ps pairs

--- image_creation/prompt_seed_creation.py
+++ image_creation/prompt_seed_creation.py
@@ -1,5 +1,17 @@
+import json
+import os
+
+from utils.paths import DATA_DIR
+
+
 def main():
-    pass
+    for n in [1, 2, 3]:
+        filepath = os.path.join(DATA_DIR, f"prompt_seed_pairs_{n}.json")
+        ps_pairs = json.load(open(filepath, "r"))
+        for i, pair in enumerate(ps_pairs):
+            del pair["id"]
+            pair["img_code"] = i
+        json.dump(ps_pairs, open(filepath, "w"), indent=2)
 
 
 if __name__ == "__main__":
